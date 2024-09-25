5cc630dc523a965f83a65c63e52f0637480deba9
LuzMieres <mieresluz5@gmail.com>
2024-09-25 12:55:24 -0300

    ajustes en formulario de newloan y agregue funcionalidad de cambio de contraseña en login

---
+++ src/redux/actions/passwordActions.js
@@ -0,0 +1,32 @@
+// passwordActions.js
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+export const changePasswordAction = createAsyncThunk(
+  "auth/changePassword",
+  async ({ currentPassword, newPassword }, { rejectWithValue }) => {
+    const token = localStorage.getItem("token");
+    if (!token) {
+      return rejectWithValue("No token available");
+    }
+
+    try {
+      const response = await axios.post(
+        "https://homebanking-c55-mh-java-luz-romina-mieres.onrender.com/api/auth/current/change-password",
+        { currentPassword, newPassword },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+
+      return response.data; // Mensaje de éxito
+    } catch (error) {
+      console.error("Error al cambiar la contraseña:", error);
+      return rejectWithValue(
+        error.response ? error.response.data : "Unknown error"
+      );
+    }
+  }
+);
